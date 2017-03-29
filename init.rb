e0d22ef86f4c584ad9eb1bfa33b5e959741ba8a9
Akihisa Higuchi <akihisa.nq@gmail.com>
2017-03-30 00:25:49 +0900

    Initial Commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :books_wiki_link do
+  name 'Books Wiki Link plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
