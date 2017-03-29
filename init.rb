36eff52be1f4b3f7d2b448b9467f322ad183d710
Akihisa Higuchi <akihisa.nq@gmail.com>
2017-03-30 02:11:14 +0900

    マクロ追加

--- init.rb
+++ init.rb
@@ -6,3 +6,20 @@ Redmine::Plugin.register :books_wiki_link do
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 end
+
+Redmine::WikiFormatting::Macros.register do
+	desc "Book link"
+	macro :book do |obj, args|
+		wiki = WikiPage.where("title = ?", args[0]).find {|page| page.project.identifier == "books" }
+		title = wiki.title
+		if wiki.text =~ /^h1\.(.*)$/o
+			title = $1.strip
+		end
+		if wiki.attachments.empty? || args[1] != "t"
+			link_to(title, wiki) + args[1]
+		else
+			thumbnail_tag(wiki.attachments.first) + ' ' + link_to(title, wiki)
+		end
+	end
+end
+
